01767770385d0d797a7babc71263002f682b08fd
Dusan Radojcic <dusan.radojcic@htecgroup.com>
2022-10-20 10:48:49 +0200

    Write readme

--- src/Register.js
+++ src/Register.js
@@ -14,11 +14,11 @@ const Register = (props) => {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer eyJraWQiOiJxMEl0SjhJdWJmMEJCSUlqcFJxOHV3ZmV3bGo2akFYd2cwaVVyaU9VM3BRPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI4ZTM2NzRlMC03YTFmLTQ2YWUtYTUyYy1kMTBmYTNjMThhYTkiLCJjb2duaXRvOmdyb3VwcyI6WyJ1cy1lYXN0LTJfNnRoWHhkQ1FLX0dvb2dsZSJdLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0yLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMl82dGhYeGRDUUsiLCJ2ZXJzaW9uIjoyLCJjbGllbnRfaWQiOiI2a2tsdjZ0Zzg4N25wbHVndW5jajZrNWs1NSIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4gcGhvbmUgb3BlbmlkIHByb2ZpbGUgZW1haWwiLCJhdXRoX3RpbWUiOjE2NjYxODIxMjgsImV4cCI6MTY2NjE4NTcyOCwiaWF0IjoxNjY2MTgyMTI4LCJqdGkiOiJlMjBhZTU2ZS03MmQxLTRlNzAtYWZhYi00NmVmMDhmMTBhOWUiLCJ1c2VybmFtZSI6Imdvb2dsZV8xMTcyMzc0MjU0Nzg1MzE3NDE1NDIifQ.OKQwVySopXDsOUBKeG9Ifh8Zd7IlKydmluPreuGalU2coNEAwxy4_35SXak-0QJyegP8gZOyvc-d8cdJWSJHDb4W6x1CyvOXxktyhc4kt1kZvzgg-DsnkXr2e9dc6d0pjEWXuuDZrBGIyThx-Evv1OkCEiNWpCg6oa6gpolEejs9MwwxeCCBiTxNoD0N__rXxDTMizeE6lSgUQ8JYNuqCKMpFfy-LoZmhEfD29-sSliZQLXpoJtd1BkguuP34yMMsmcHJPYnFZnyuqr9VhEt4YTPYGOlfMEc1W-GngpYv1C_dbCD6X7axiGTK1ObVfIgbAi1fCdwkRTd6GtdgWzL8w'
+        'Authorization': 'Bearer ACCESS_TOKEN'
       },
       body: JSON.stringify({
         // email: email,
-        email: 'dusanradojcic00@gmail.com',
+        email: 'change_email_here@gmail.com',
         firstName: 'User',
         lastName: 'Userson',
         // password: 'Password1!',
